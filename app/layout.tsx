14408af93b26caea2f84e1532ec1a4dd3c5b7061
Ratiporn <ratiporn.pr@gmail.com>
2023-11-29 16:17:52 +0700

    add auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
+import { Libre_Barcode_39_Extended_Text, Nunito } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./components/providers/ToasterProvider";
+// import { Toaster } from "react-hot-toast";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -16,7 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+        <ToasterProvider />
+        <RegisterModal />
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
